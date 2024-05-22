673769bab53ae211282a96524ebd30841651277b
AghilesC <chaouche.aghiles@gmail.com>
2024-05-22 12:08:09 -0400

    aghiles to ari

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Aghiles</h1>
+      <h1>ari</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
