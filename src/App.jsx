c34ab6d4d3b49a36949082a0f4836b9daa6bd6fe
AghilesC <chaouche.aghiles@gmail.com>
2024-05-24 12:02:25 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-
+import { useState } from "react"
 
 function App() {
 
+const [prenom, setPrenom] = useState()
 
 
 
+const handleClick = () => {
+setPrenom = alert("Bonjour", {prenom})
 
-
+}
 
 return ( 
 <div>
   <h1>Bienvenue chez nous !</h1><br />
   <h2>Connectez-vous</h2>
-  <input type="text" placeholder="Entrez votre prénom" required/>
-  <button type="submit">Accédez à votre espace</button>
+  <input type="text" placeholder="Entrez votre prénom" required />
+  <button type="submit" onClick={handleClick}>Accédez à votre espace</button>
 
 
 
