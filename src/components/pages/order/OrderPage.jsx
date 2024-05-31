efb08070ddda673a8b4f1353ada25edcea06313e
AghilesC <chaouche.aghiles@gmail.com>
2024-05-31 10:24:00 -0400

    🚩 F02: APP > Installation du routing system

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,16 +1,3 @@
-import React from 'react'
-import LoginForm from '../login/LoginForm'
-
 export default function OrderPage() {
-
-
-
-
-
-
-
-
-  return (
-    <div>Bonjour</div>
-  )
+  return "Bonjour"
 }
