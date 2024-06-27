dcc7897bd88fe188045ce35b825b4f69b4415c1f
Aghiles <chaouche.aghiles@gmail.com>
2024-06-26 21:30:05 -0400

    Sauvegarde styled components LOGIN

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,29 +1,140 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { IoChevronForward } from "react-icons/io5";
+import { BsPersonCircle } from "react-icons/bs";
+
 export default function LoginForm() {
+  const [inputValue, setInputValue] = useState("");
+  const navigate = useNavigate();
 
+  const handleSubmit = (event) => {
+    navigate(`order/${inputValue}`);
+  };
 
-const [inputValue, setInputValue] = useState("")
-const navigate = useNavigate()
-
-const handleSubmit = (event) => { 
-navigate(`order/${inputValue}`)
- }
-
-const handleChange = (event) => { 
-    setInputValue(event.target.value)
-
-    
- }
-
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
   return (
-    <form action="submit" onSubmit={handleSubmit}>
-    <h1>Bienvenue chez nous !</h1>
-    <br />
-    <h2 >Connectez-vous</h2>
-    <input type="text" value={inputValue} onChange={handleChange} placeholder="Entrez votre prénom" required />
-    <button type="submit">Accédez à votre espace</button>
-</form>
-  )
+    <LoginFormStyled action="submit" onSubmit={handleSubmit}>
+      <div>
+        <h1>Bienvenue chez nous !</h1>
+        <hr />
+        <h2>Connectez-vous</h2>
+      </div>
+      <div className="input-with-icon">
+        <BsPersonCircle className="icon" />
+        <input
+          value={inputValue}
+          onChange={handleChange}
+          type="text"
+          placeholder="Entrez votre prénom "
+        />
+      </div>
+      <button className="button-with-icon" type="submit">
+        <span>Accédez à votre espace</span>
+        <IoChevronForward />
+      </button>
+    </LoginFormStyled>
+  );
 }
+
+const LoginFormStyled = styled.form`
+  text-align: center;
+  max-width: 500px;
+  min-width: 400px;
+  margin: 0px auto;
+  padding: 2.5rem 2rem;
+  border-radius: 5px;
+  font-family: "Amatic SC", cursive;
+
+  hr {
+    border: 1.5px solid #f56a2c;
+    margin-bottom: 40px;
+  }
+
+  h1 {
+    color: white;
+    font-size: 48px;
+  }
+
+  h2 {
+    color: #8e8b8b;
+    margin: 20px 10px 10px;
+    color: white;
+    font-size: 36px;
+  }
+
+  .input-with-icon {
+    background-color: #fff;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    padding: 18px 24px;
+    margin: 18px 0;
+  }
+
+  .icon {
+    font: 15px;
+    margin-right: 8px;
+    color: #93a2b1;
+  }
+
+  input {
+    border: none;
+    font-size: 15px;
+    color: #17161a;
+  }
+
+  &::placeholder {
+    background: white;
+    color: lightgray;
+  }
+
+  .button-with-icon {
+    width: 100%;
+    border: 1px solid red;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    white-space: nowrap;
+    text-decoration: none;
+    line-height: 1;
+
+    padding: 18px 24px;
+    border-radius: 5px;
+    font-size: 15px;
+    font-weight: 800;
+    color: white;
+    background-color: #ff9f1b;
+    border: 1px solid #ff9f1b;
+
+    &:hover:not(:disabled) {
+      background-color: white;
+      color: #ff9f1b;
+      border: 1px solid #ff9f1b;
+      transition: all200ms ease-out;
+    }
+
+    &:active {
+      color: white;
+      background-color: #ff9f1b;
+      border: 1px solid #ff9f1b;
+    }
+
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    .icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 15px;
+      margin-left: 10px;
+    }
+  }
+`;
--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,9 +1,35 @@
-import LoginForm from "./LoginForm"
-
+import Logo from "../../reusable-ui/Logo";
+import LoginForm from "./LoginForm";
+import styled from "styled-components";
 export default function LoginPage() {
-
-
-return <LoginForm />
-
+  return (
+    <LoginPageStyled>
+      <Logo />
+      <LoginForm />
+    </LoginPageStyled>
+  );
 }
 
+const LoginPageStyled = styled.div`
+  height: 97vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  &::before {
+    content: "avant";
+
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+      url(${"/images/burger-background.jpg"});
+    background-size: cover;
+    background-position: center;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+`;
