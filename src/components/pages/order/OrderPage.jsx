dbc4d2f2cfce418689649a101a7a71a35b5f4a47
AghilesC <chaouche.aghiles@gmail.com>
2024-07-03 12:49:12 -0400

    💄  F04: ORDERPAGE > Add layout and container

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,16 +1,34 @@
 import { Link, useParams } from "react-router-dom";
-
+import styled from 'styled-components';
 export default function OrderPage() {
  
 const {username} = useParams()
 
  
  
- return <div>
-    <h1>Bonjour {username}</h1>
-    <Link to="/">
-      <button>Deconnexion</button>
-    </Link>
-
-  </div>
+ return <OrderPageStyled>
+    <div className="container">
+      <h1>Bonjour {username}</h1>
+      <Link to="/">
+        <button>Deconnexion</button>
+      </Link>
+    </div>
+  </OrderPageStyled>
 }
+
+
+
+const OrderPageStyled = styled.div`
+  background-color: orange;
+  height: 98.3vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container {
+    background-color: red;
+    height: 95vh;
+    width: 1400px;
+  }
+`;
+
