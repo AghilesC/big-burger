6cb8d1843d9d084a813fa20fb07e17d31105f585
AghilesC <chaouche.aghiles@gmail.com>
2024-07-04 12:32:59 -0400

    💄 F04: ORDERPAGE > Add navbar and name

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,22 +1,23 @@
 import { Link, useParams } from "react-router-dom";
-import styled from 'styled-components';
+import styled from "styled-components";
 export default function OrderPage() {
- 
-const {username} = useParams()
+  const { username } = useParams();
 
- 
- 
- return <OrderPageStyled>
-    <div className="container">
-      <h1>Bonjour {username}</h1>
-      <Link to="/">
-        <button>Deconnexion</button>
-      </Link>
-    </div>
-  </OrderPageStyled>
+  return (
+    <OrderPageStyled>
+      <div className="container">
+        <div className="navbar">
+          Navbar
+          <h1>Bonjour {username}</h1>
+          <Link to="/">
+            <button>Deconnexion</button>
+          </Link>
+        </div>
+        <div className="main">Main</div>
+      </div>
+    </OrderPageStyled>
+  );
 }
-
-
 
 const OrderPageStyled = styled.div`
   background-color: orange;
@@ -29,6 +30,17 @@ const OrderPageStyled = styled.div`
     background-color: red;
     height: 95vh;
     width: 1400px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .navbar {
+    background-color: blue;
+    height: 10vh;
+  }
+
+  .main {
+    background-color: green;
+    flex: 1%;
   }
 `;
-
