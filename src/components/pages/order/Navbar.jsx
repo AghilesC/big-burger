8fb25caa7650784ee9228117f678df5a6c695c70
AghilesC <chaouche.aghiles@gmail.com>
2024-07-04 14:39:36 -0400

    ♻️ F04: ORDERPAGE > Navbar left and right side

--- src/components/pages/order/Navbar.jsx
+++ src/components/pages/order/Navbar.jsx
@@ -5,16 +5,29 @@ import styled from "styled-components";
 export default function Navbar({ username }) {
   return (
     <NavbarStyled>
-      Navbar
-      <h1>Bonjour {username}</h1>
-      <Link to="/">
-        <button>Deconnexion</button>
-      </Link>
+      <div className="left-side"> Left </div>
+      <div className="right-side">
+        Right
+        <h1>Bonjour {username}</h1>
+        <Link to="/">
+          <button>Deconnexion</button>
+        </Link>
+      </div>
     </NavbarStyled>
   );
 }
 
 const NavbarStyled = styled.nav`
-  background-color: blue;
+  background: blue;
   height: 10vh;
+  display: flex;
+  justify-content: space-between;
+
+  .right-side {
+    background: purple;
+  }
+
+  .left-side {
+    background: pink;
+  }
 `;
