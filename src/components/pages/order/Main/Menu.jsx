e0710530dafcfab85bded0aaa8ba624b4c891297
AghilesC <chaouche.aghiles@gmail.com>
2024-07-15 10:27:41 -0400

    🍱 F05: Add assets

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 export default function Menu() {
+  const [products, setProducts] = useState(second);
+
   return <MenuStyled>Menu</MenuStyled>;
 }
 
 const MenuStyled = styled.div`
-  background: green;
+  background: purple;
 `;
