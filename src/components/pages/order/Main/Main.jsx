71704958d09b5b8ef376c6d2a36c923d1f917a5e
Aghiles <chaouche.aghiles@gmail.com>
2024-07-28 21:12:19 -0400

    💄 F07: MAIN > Add layout with Basket

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -8,19 +8,36 @@ export default function Main() {
   return (
     <MainStyled>
       {/* <Basket /> */}
-      <Menu />
+      <div className="menu-and-admin">
+        <Menu />
+        <div className="admin">admin</div>
+      </div>
     </MainStyled>
   );
 }
 
 const MainStyled = styled.div`
-  flex: 1%;
+  height: calc(95vh - 10vh);
   background: ${theme.colors.background_white};
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   display: grid;
-  grid-template-columns: 100%;
+  grid-template-columns: 1fr;
 
   overflow-y: scroll;
+
+  .menu-and-admin {
+    position: relative;
+    overflow-y: hidden;
+    display: grid;
+    .admin {
+      background: red;
+      position: absolute;
+      height: 250px;
+      bottom: 0;
+      left: 0;
+      right: 0;
+    }
+  }
 `;
